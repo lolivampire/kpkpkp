acc7681550c5fd9907c4bf69348970082e03d618
Yotsubros <76827603+lolivampire@users.noreply.github.com>
2022-06-24 08:49:18 +0700

    Add files via upload

--- web/user.php
+++ web/user.php
@@ -1,13 +1,15 @@
 <?php
 
 include_once "../web/koneksi.php";
+require_once "../web/config/functions.php";
 
+$fun = new Functions();
 
-$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user ORDER BY us.id_user ASC";
-$option_kelas = "SELECT * FROM kelas";
+// $sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND  ORDER BY us.id_user ASC";
+// $option_kelas = "SELECT * FROM kelas";
 
-$result = mysqli_query($conn, $sql);
-$result_kelas = mysqli_query($conn,$option_kelas);
+// $result = mysqli_query($conn, $sql);
+// $result_kelas = mysqli_query($conn, $option_kelas);
 
 ?>
 
@@ -25,7 +27,7 @@ $result_kelas = mysqli_query($conn,$option_kelas);
     <title>Master User</title>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js">
-        
+
     </script>
 </head>
 
@@ -33,19 +35,15 @@ $result_kelas = mysqli_query($conn,$option_kelas);
     <div class="d-flex" id="wrapper">
         <!-- Sidebar -->
         <div class="bg-white" id="sidebar-wrapper">
-            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i
-                    class="fas fa-user-secret me-2"></i>SIPN</div>
+            <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fas fa-user-secret me-2"></i>SIPN</div>
             <div class="list-group list-group-flush my-3">
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active"><i
-                        class="fas fa-project-diagram me-2"></i>User</a>
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-chart-line me-2"></i>Mapel</a>
-                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i
-                        class="fas fa-paperclip me-2"></i>Nilai</a>
-                <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i
-                        class="fas fa-power-off me-2"></i>Logout</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
+                <a href="../web/user.php" class="list-group-item list-group-item-action bg-transparent second-text active"><i class="fas fa-project-diagram me-2"></i>User Admin</a>
+                <a href="user-guru.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-project-diagram me-2"></i>User Guru</a>
+                <a href="../web/user-siswa.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-project-diagram me-2"></i>User Siswa</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-chart-line me-2"></i>Mapel</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-paperclip me-2"></i>Nilai</a>
+                <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i class="fas fa-power-off me-2"></i>Logout</a>
             </div>
         </div>
         <!-- /#sidebar-wrapper -->
@@ -54,22 +52,19 @@ $result_kelas = mysqli_query($conn,$option_kelas);
         <div id="page-content-wrapper">
             <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
                 <div class="d-flex align-items-center">
-                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
+                    <em class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></em>
                     <h2 class="fs-2 m-0">Master User</h2>
                 </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="fas fa-user me-2"></i>John Doe
+                            <a class="nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <em class="fas fa-user me-2"></em>John Doe
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                                 <li><a class="dropdown-item" href="#">Profile</a></li>
@@ -83,64 +78,69 @@ $result_kelas = mysqli_query($conn,$option_kelas);
 
             <!-- PREVIEW CARD -->
             <div class="container-fluid px-4">
-                
-                <!-- PREVIEW CARD -->
-                
-                
-                <div class="d-flex mb-3">
-                  <div class="p-2"> 
-                        <select class="form-select" aria-label="Default select example" id="pilih_role">
-                          <option selected value="0">Semua Role</option>
-                          <option value="1">Admin</option>
-                          <option value="2">Guru</option>
-                          <option value="3">Siswa</option>
-                        </select>      
-                  </div>
-                  <div class="p-2"> 
-                        <select class="form-select" aria-label="Default select example" id="pilih_kelas">
-                          <option selected value="0">Semua Kelas</option>
-                           <?php while($row=mysqli_fetch_assoc($result_kelas)): ?>
-                          <option value=" <?php echo $row['id_kelas'] ?> "> <?php echo $row['nama'] ?>  </option>
-                          <?php endwhile; ?>
-                        </select> 
-                    </div>
-                  <div class="ms-auto p-2">
-                         <a href="tambah-data-user.php" class="btn btn-success float-start">Tambah Data</a> 
-                  </div>
-                </div>
 
+                <!-- PREVIEW CARD -->
+
+
+                <div class="d-flex mb-3">
+                    <div class="p-2">
+                        <select class="form-select" aria-label="Default select example" id="pilih_role">
+                            <option selected value="0">Semua Role</option>
+                            <option value="1">Admin</option>
+                            <option value="2">Guru</option>
+                            <option value="3">Siswa</option>
+                        </select>
+                    </div>
+                    <div class="p-2">
+                        <select class="form-select" aria-label="Default select example" id="pilih_kelas">
+                            <option selected value="0">Semua Kelas</option>
+                            <?php while ($row = mysqli_fetch_assoc($result_kelas)) : ?>
+                                <option value=" <?php echo $row['id_kelas'] ?> "> <?php echo $row['nama'] ?> </option>
+                            <?php endwhile; ?>
+                        </select>
+                    </div>
+                    <div class="ms-auto p-2">
+                        <a href="tambah-data-user.php" class="btn btn-success float-start">Tambah Data</a>
+                    </div>
+                </div>
                 <div class="row my-3">
                     <h3 class="fs-4 mb-3">User</h3>
                     <div class="col">
                         <table class="table bg-white rounded shadow-sm  table-hover" id='tabel_user'>
-                            <!-- <thead>
+                            <thead>
                                 <tr>
                                     <th scope="col" width="10%">ID</th>
                                     <th scope="col" width="20%">Nama</th>
                                     <th scope="col" width="10%">Email</th>
                                     <th scope="col" width="10%">Password</th>
-                                    <th scope="col"width="10%">Hak Akses</th>
-                                    <th scope="col"width="20%">Date Created</th>
+                                    <th scope="col" width="10%">Hak Akses</th>
+                                    <th scope="col" width="20%">Date Created</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
-                            <tbody> -->
+                            <tbody>
 
-                               <!--  <?php while($row=mysqli_fetch_assoc($result)): ?>
-                                <tr>
-                                    <th scope="row"> <?= $row["ID"]; ?> </th>
-                                    <td> <?= $row["nama"]; ?> </td>
-                                    <td> <?= $row["email"]; ?> </td>
-                                    <td> <?= $row["Pass"]; ?> </td>
-                                    <td> <?= $row["hak_akses"]; ?> </td>
-                                    <td> <?= $row["date_created"]; ?> </td>
-                                    <td>
-                                        <a href="#" class="btn btn-warning">Rincian</a> 
-                                        <a href="#" class="btn btn-primary">Edit</a>
-                                        <a href="#" class="btn btn-danger">Hapus</a>  
-                                     </td>
-                                </tr>
-                                <?php endwhile; ?> -->
+                                <?php
+                                $result = $fun->getUserAdmin();
+                                if ($result && $result->num_rows > 0) {
+                                    while ($row = $result->fetch_assoc()) { ?>
+                                        <tr>
+                                            <th scope="row"> <?= $row["ID"]; ?> </th>
+                                            <td> <?= $row["nama"]; ?> </td>
+                                            <td> <?= $row["email"]; ?> </td>
+                                            <td> <?= $row["Pass"]; ?> </td>
+                                            <td> <?php if ($row["hak_akses"] == 1) {
+                                                        echo "Admin";
+                                                    }; ?> </td>
+                                            <td> <?= $row["date_created"]; ?> </td>
+                                            <td>
+                                                <a href="#" class="btn btn-warning">Rincian</a>
+                                                <a href="#" class="btn btn-primary">Edit</a>
+                                                <a href="#" class="btn btn-danger">Hapus</a>
+                                            </td>
+                                        </tr>
+                                <?php }
+                                } ?>
 
                             </tbody>
                         </table>
@@ -158,103 +158,51 @@ $result_kelas = mysqli_query($conn,$option_kelas);
         var el = document.getElementById("wrapper");
         var toggleButton = document.getElementById("menu-toggle");
 
-        toggleButton.onclick = function () {
+        toggleButton.onclick = function() {
             el.classList.toggle("toggled");
         };
 
-        $(document).ready(function(){
+        // $(document).ready(function() {
 
-            var kelas, role;
+        //     $('#pilih_role').change(function() {
+        //         $(this).find('option:selected').each(function() {
+        //             var role = $(this).attr('value');
+        //             if (role == 1 || role == 2 || role == 0) {
+        //                 $('#pilih_kelas').attr('disabled', 'disabled');
+        //             } else {
+        //                 $('#pilih_kelas').removeAttr("disabled");
+        //             }
 
-            $('#pilih_role').change(function(){
-                $(this).find('option:selected').each(function(){
-                    role = $(this).attr('value');
-                    $('#pilih_kelas').change(function(){
-                        $(this).find('option:selected').each(function(){
-                            kelas = $(this).attr('value');
-                    });
-                        if (role == 3) {
-                            $('#pilih_kelas').removeAttr("disabled");
-                            $.ajax({
-                                type: 'POST',
-                                url: 'tabel.php',
-                                data: {
-                                    tampilData: 'siswa',
-                                    pilihRole: role,
-                                    pilihKelas: kelas
-                                },
-                                success: function(data){
-                                    //$('#tabel_user').html(data);
-                                    console.log(data);
-                                }                          
-                            });
-                        } else {
-                            $('#pilih_kelas').attr('disabled','disabled');
-                            $('#pilih_kelas').val(0);
-                            $.ajax({
-                                type: 'POST',
-                                url: 'tabel.php',
-                                data: {
-                                    tampilData: 'tampil',
-                                    pilihRole: role
-                                },
-                                success: function(data){
-                                    //$('#tabel_user').html(data);
-                                    console.log(data);
-                                }                          
-                            });
-                        }
-                }).change();
-            });
-            
-            // $('#pilih_role').change(function(){
-            //     $(this).find('option:selected').each(function(){
-            //         var role = $(this).attr('value');
-            //         if (role==1 || role==2 || role==0) {
-            //             $('#pilih_kelas').attr('disabled','disabled');
-            //         } else {
-            //             $('#pilih_kelas').removeAttr("disabled");
-            //             if (role==3) {
+        //             $.ajax({
+        //                 type: 'POST',
+        //                 url: 'tabel.php',
+        //                 data: 'rolePilih=' + role,
+        //                 success: function(data) {
+        //                     $('#tabel_user').html(data);
+        //                 }
+        //             });
 
-            //                 $('#pilih_kelas').change(function(){
-            //                     $(this).find('option:selected').each(function(){
-            //                     var kelas = $(this).attr('value');
-            //                     alert(kelas);
-            //                     if (kelas != 0) {
-            //                         $.ajax({
-            //                             type: 'POST',
-            //                             url: 'tabel.php',
-            //                             data: {
-            //                                 tabelsiswa: 'kelas',
-            //                                 kelasPilih: kelas
-            //                             },
-            //                             success: function(data){
-            //                                 $('#tabel_user').html(html);
-            //                             }
-            //                         });
-                           
-            //                     }
-            //                 });
-            //         }).change();
-            //             }
-            //         }
 
-            //         $.ajax({
-            //                 type: 'POST',
-            //                 url: 'tabel.php',
-            //                 data: 'rolePilih='+role,
-            //                 success: function(data){
-            //                 $('#tabel_user').html(data);
-            //             }
-            //         });
+        //         });
+        //         $('#pilih_kelas').change(function() {
+        //             $(this).find('option:selected').each(function() {
+        //                 var kelas = $(this).attr('value');
+        //                 if (kelas != 0) {
+        //                     alert(kelas);
+        //                     $.ajax({
+        //                         type: 'POST',
+        //                         url: 'tabel.php',
+        //                         data: 'kelasPilih=' + kelas,
+        //                         success: function(data) {
+        //                             $('#tabel_user').html(data);
+        //                         }
+        //                     });
+        //                 }
+        //             });
+        //         });
+        //     }).change();
 
-                    
-            //     });
-                 
-            // }).change();
-           
-        }).change();
-        });
+        // });
     </script>
 </body>
 
