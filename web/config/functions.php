1cc36f93f79c623dfbc2579a22ecb4dc71c7f714
Yotsubros <76827603+lolivampire@users.noreply.github.com>
2022-06-24 08:50:00 +0700

    Add files via upload

--- web/config/functions.php
+++ web/config/functions.php
@@ -1,19 +1,47 @@
-<?php 
+<?php
 
-function getUserData($id) {
+include_once "../web/koneksi.php";
 
-	$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND us.hak_akses ='$id' ORDER BY us.id_user ASC";
-	return $sql;
-} 
+class Functions
+{
+	function getUserData($id)
+	{
 
-function getAllUser(){
-	$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user  ORDER BY us.id_user ASC";
-	return $sql;
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND us.hak_akses ='$id' ORDER BY us.id_user ASC";
+		return $sql;
+	}
+
+	function getAllUser()
+	{
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user  ORDER BY us.id_user ASC";
+		return $sql;
+	}
+
+	function getSiswaKelas($id)
+	{
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND du.id_kelas = '$id'  ORDER BY us.id_user ASC";
+		return $sql;
+	}
+
+	function getUserAdmin()
+	{
+		global $conn;
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND us.hak_akses = 1  ORDER BY us.id_user ASC";
+		$result = $conn->query($sql);
+		return $result;
+	}
+
+	function getUserGuru()
+	{
+		global $conn;
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND us.hak_akses = 2  ORDER BY us.id_user ASC";
+		$result = $conn->query($sql);
+		return $result;
+	}
+	function getUserSiswa(){
+		global $conn;
+		$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND us.hak_akses = 3  ORDER BY us.id_user ASC";
+		$result = $conn->query($sql);
+		return $result;
+	}
 }
-
-function getSiswaKelas($id){
-	$sql = "SELECT us.id_user AS ID, du.nama, us.email, us.password AS Pass, us.hak_akses, us.date_created FROM user_sistem us, detail_user du WHERE us.id_user=du.id_user AND du.id_kelas = '$id'  ORDER BY us.id_user ASC";
-	return $sql;
-}
-
- ?>
